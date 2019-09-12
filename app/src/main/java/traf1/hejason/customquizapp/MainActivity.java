6dbeeb6c93eb2f19b99094d0a5d3369689b35851
jasonhe.md@gmail.com <Hejiachen2003>
2019-09-12 10:03:34 -0400

    Start of math idea and addition of spinner

--- app/src/main/java/traf1/hejason/customquizapp/MainActivity.java
+++ app/src/main/java/traf1/hejason/customquizapp/MainActivity.java
@@ -3,6 +3,7 @@ package traf1.hejason.customquizapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
+import android.app.Activity;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -12,9 +13,12 @@ import android.os.Bundle;
 import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,7 +26,9 @@ public class MainActivity extends AppCompatActivity {
     private int highScore;
     Button submitButton,reset;
     TextView textView, scoreBoard;
+    EditText number1, number2;
     Chronometer cmTimer;
+    Spinner dropdown;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,8 +44,11 @@ public class MainActivity extends AppCompatActivity {
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                highScore++;
-                scoreBoard.setText("Score: " + highScore);
+                if(verify()) {
+                    highScore++;
+                    scoreBoard.setText("Score: " + highScore);
+                }
+                else
             }
         });
 
@@ -59,8 +68,51 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        number1 = findViewById(R.id.FirstNumber);
+        number2 = findViewById(R.id.SecondNumber);
+
+        dropdown = findViewById(R.id.operations_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.operations_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        dropdown.setOnItemSelectedListener(SpinnerActivity);
+
+    }
+    public class SpinnerActivity extends Activity implements AdapterView.OnItemSelectedListener {
+
+        public void onItemSelected(AdapterView<?> parent, View view,
+                                   int pos, long id) {
+            // An item was selected. You can retrieve the selected item using
+            parent.getItemAtPosition(pos);
+        }
+
+        public void onNothingSelected(AdapterView<?> parent) {
+            // Another interface callback
+        }
     }
 
+    public int generateNumber(View view){
+        return (int)(Math.random() * 100);
+    }
+
+    public boolean verify(int result, int number1, int number2, char operation){
+        switch (operation){
+            case '+':
+                return number1 + number2 == result;
+            case '-':
+                return number1 - number2 == result;
+            case '*':
+                return number1 * number2 == result;
+            case '/':
+                return number1 / number2 == result;
+            case '%':
+                return number1 % number2 == result;
+        }
+        return false;
+    }
     public void toastSomething(View view){
         String text = "Hello world!";
         Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
